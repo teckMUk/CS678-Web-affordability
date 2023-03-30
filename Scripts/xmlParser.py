c72b4fc22aa68c3ffe3663b40318a52f72edcd07
SaadMaybe <88343088+SaadMaybe@users.noreply.github.com>
2023-03-30 18:09:59 +0500

    pakistan.gov.pk
    xml and txt file

--- Scripts/xmlParser.py
+++ Scripts/xmlParser.py
@@ -22,4 +22,4 @@ def filter_urls_above_priority(filename, priority_threshold):
         for url in urls_above_priority:
             f.write(url + '\n')
 
-filter_urls_above_priority('moib.gov.pk.xml', 0.8)
+filter_urls_above_priority('sites\pakistan.gov.pk.xml', 0.8)
