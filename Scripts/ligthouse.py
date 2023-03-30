92879096cca2e839ca3a010f684a0b006503956f
SaadMaybe <88343088+SaadMaybe@users.noreply.github.com>
2023-03-31 03:26:25 +0500

    9 more websites

--- Scripts/ligthouse.py
+++ Scripts/ligthouse.py
@@ -1,5 +1,8 @@
 import requests
 import os
+import json
+from tqdm import tqdm
+import time
 
 def get_lighthouse_score(url):
     base_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url='
@@ -8,8 +11,25 @@ def get_lighthouse_score(url):
     return res.json()
 
 sites_dir = os.path.join(os.getcwd(), 'sites')
+
 files = os.listdir(sites_dir)
 
 URLs = [x.replace('.xml', '') for x in files if x.endswith('.xml')]
 
-print(get_lighthouse_score(URLs[0]))
+newdict = {}
+
+with open('Scripts\lighthouse_done.txt', 'r') as f:
+    done = f.read().splitlines()
+    URLs = [x for x in URLs if x not in done]
+
+for i in tqdm(range(len(URLs))):
+    newdict[URLs[i]] = get_lighthouse_score(URLs[i])
+
+    
+    with open('\Scripts\lighthouse_results\\'+ URLs[i]+'.json', 'w') as f:
+        json.dump(newdict[URLs[i]], f)
+        time.sleep(5)
+
+    # append the URL to the lighthouse_done.txt file
+    with open('Scripts\lighthouse_done.txt', 'a') as f:
+        f.write(URLs[i] + '\n')
--- Scripts/xmlParser.py
+++ Scripts/xmlParser.py
@@ -18,4 +18,4 @@ def filter_urls_above_priority(filename, priority_threshold):
         for url in urls_above_priority:
             f.write(url + '\n')
 
-filter_urls_above_priority('sites\mocc.gov.pk.xml', 0.8)
+filter_urls_above_priority('sites\mohw.gov.pk.xml', 0.8)
